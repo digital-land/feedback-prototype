7d16cf94d13ad34baf4bfafe721ebcdc82b21e73
Paul Smith <mail@paulsmith.site>
2022-11-17 16:49:03 +0000

    adds basic layout and page structure set-up

--- application/blueprints/main/views.py
+++ application/blueprints/main/views.py
@@ -5,4 +5,11 @@ main = Blueprint("main", __name__, template_folder="templates")
 
 @main.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("homepage.html")
+
+
+@main.route("/organisation/<organisation>")
+def organisation(organisation):
+    return render_template(
+        "organisation.html", routeData={"organisation": organisation}
+    )
