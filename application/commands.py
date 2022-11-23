b321455a19029956cd3f66bace644b0202fcc050
Adam Shimali <adam.shimali@gmail.com>
2022-11-23 18:51:34 +0000

    first cut of trying to get entities for datasets in a reasonable amount of time

--- application/commands.py
+++ application/commands.py
@@ -1,7 +1,10 @@
+import concurrent
 import logging
+import urllib.parse
 
 import requests
 from flask.cli import AppGroup
+from sqlalchemy import delete
 from sqlalchemy.dialects.postgresql import insert
 
 from application.extensions import db
@@ -30,6 +33,20 @@ SELECT dataset, name, text
 FROM dataset;
 """
 
+# temp use only these datasets
+RIPA_DATASETS = set(
+    [
+        "article-4-direction",
+        "article-4-direction-area",
+        "conservation-area",
+        "listed-building",
+        "listed-building-outline",
+        "tree-preservation-order",
+        "tree-preservation-zone",
+        "tree",
+    ]
+)
+
 
 @data_cli.command("load")
 def load_data():
@@ -37,12 +54,10 @@ def load_data():
     from flask import current_app
 
     print("load organisations")
-
     datasette_url = current_app.config["DATASETTE_URL"]
     url = (
         f"{datasette_url}/digital-land.json?sql={organisation_sql.strip()}&_shape=array"
     )
-
     resp = requests.get(url)
     resp.raise_for_status()
     data = resp.json()
@@ -66,20 +81,29 @@ def load_data():
     print("load organisations done")
 
     print("load datasets")
-    url = f"{datasette_url}/digital-land.json?sql={dataset_sql.strip()}&_shape=array"
-    resp = requests.get(url)
-    resp.raise_for_status()
-    data = resp.json()
+    # url = f"{datasette_url}/digital-land.json?sql={dataset_sql.strip()}&_shape=array"
+    # resp = requests.get(url)
+    # resp.raise_for_status()
+    # data = resp.json()
+    # inserts = []
+    # for dataset in data:
+    #     inserts.append(
+    #         {
+    #             "dataset": dataset["dataset"],
+    #             "name": dataset["name"],
+    #             "text": dataset["text"],
+    #         }
+    #     )
+
+    # tmp use ripa datasets only
     inserts = []
-    for dataset in data:
+    for dataset in RIPA_DATASETS:
         inserts.append(
             {
-                "dataset": dataset["dataset"],
-                "name": dataset["name"],
-                "text": dataset["text"],
+                "dataset": dataset,
+                "name": dataset.replace("-", " ").capitalize(),
             }
         )
-
     stmt = insert(Dataset).values(inserts)
     stmt = stmt.on_conflict_do_update(
         index_elements=[Dataset.dataset], set_=dict(name=stmt.excluded.name)
@@ -87,3 +111,52 @@ def load_data():
     db.session.execute(stmt)
     db.session.commit()
     print("load datasets")
+
+
+@data_cli.command("drop")
+def drop_data():
+    stmt = delete(Dataset)
+    db.session.execute(stmt)
+    stmt = delete(Organisation)
+    db.session.execute(stmt)
+    db.session.commit()
+    print("data deleted")
+
+
+@data_cli.command("report")
+def report():
+    from flask import current_app
+
+    datasette_url = current_app.config["DATASETTE_URL"]
+
+    print("generate report")
+    datasets = Dataset.query.all()
+    request_data = [
+        {"organisation": organisation, "datasets": [ds.dataset for ds in datasets]}
+        for organisation in Organisation.query.all()
+    ]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+        futures = {
+            executor.submit(__fetch_data, datasette_url, data) for data in request_data
+        }
+        for future in concurrent.futures.as_completed(futures):
+            print(f"The outcome is {future.result()}")
+
+
+def __fetch_data(datasette_url, data):
+    organisation = data["organisation"]
+    datasets = data["datasets"]
+    datasets_url_encoded = urllib.parse.quote(",".join(datasets))
+    resp = requests.head(datasette_url)
+    url = f"{datasette_url}/entity/entity.json?organisation_entity__exact={organisation.entity}&_shape=array&dataset__in={datasets_url_encoded}&_nocol=geometry_geom&_nocol=point_geom"  # noqa
+    if resp.status_code == 200:
+        print(f"Querying {url}")
+        resp = requests.get(url)
+        resp.raise_for_status()
+        data = resp.json()
+        for d in data:
+            print(d)
+    else:
+        print("No data")
+    return f"Done fetching data for {organisation.organisation}"
