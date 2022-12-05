65246837950cbe13afaba0b7e2a114624054e63e
Paul Smith <mail@paulsmith.site>
2022-12-05 12:14:30 +0000

    updates with start sub dataset part

--- application/blueprints/designs/views.py
+++ application/blueprints/designs/views.py
@@ -11,22 +11,41 @@ def get_param_val(query_param):
     return request.args.get(query_param) if request.args.get(query_param) else False
 
 
-@designs.route("/designs/provider")
-def provider():
+@designs.route("/designs/provider", strict_slashes=False)
+@designs.route("/designs/provider/<provider>/")
+def provider(provider):
     return render_template(
         template_path("provider.html"),
         page_data={
             "organisation": {
-                "name": get_param_val("organisation")
-                if get_param_val("organisation")
-                else "Borchester Borough Council"
+                "name": provider if provider else "Borchester Borough Council"
             },
             "title": get_param_val("title")
             if get_param_val("title")
+            else provider
+            if provider
             else "Borchester Borough Council",
-            "caption": get_param_val("caption")
-            if get_param_val("caption")
-            else "Data Provider",
+            "caption": get_param_val("caption") if get_param_val("caption") else False,
+            "summary": {"show": True},
+            "query_string": request.args.to_dict(),
+            "status": ["Provided", "Unavailable", "Not found", "Problem collecting"],
+        },
+    )
+
+
+@designs.route("/designs/provider/<provider>/<dataset>")
+def provider_dataset(provider, dataset):
+    return render_template(
+        template_path("provider-dataset.html"),
+        page_data={
+            "organisation": {
+                "name": provider if provider else "Borchester Borough Council"
+            },
+            "dataset": {"name": dataset},
+            "title": dataset if dataset else "Dataset name",
+            "lede": "Data provided by " + provider
+            if provider
+            else "Borchester Borough Council",
             "summary": {"show": True},
             "query_string": request.args.to_dict(),
             "status": ["Provided", "Unavailable", "Not found", "Problem collecting"],
