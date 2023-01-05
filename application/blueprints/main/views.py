86a6092bd2b7f7117430f138a31b953ee3d216f0
Adam Shimali <adam.shimali@gmail.com>
2023-01-05 14:46:42 +0000

    Add map of letters to organisations to index page

--- application/blueprints/main/views.py
+++ application/blueprints/main/views.py
@@ -1,3 +1,5 @@
+import string
+
 from flask import Blueprint, render_template
 
 from application.models import Organisation
@@ -7,7 +9,13 @@ main = Blueprint("main", __name__, template_folder="templates")
 
 @main.route("/")
 def index():
+
     organisations = Organisation.query.order_by(Organisation.name).all()
+    organisations_by_letter = dict.fromkeys(string.ascii_lowercase, [])
+    for o in organisations:
+        key = o.name[0].lower()
+        organisations_by_letter[key].append(o)
+
     return render_template(
         "homepage.html",
         organisations=organisations,
